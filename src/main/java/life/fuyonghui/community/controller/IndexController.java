7ff6053ab2128b6170fa30602195962dd2ad8897
wfufu <fu8926@qq.com>
2020-03-09 23:48:50 +0800

    add index and bootstrap

--- src/main/java/life/fuyonghui/community/controller/IndexController.java
+++ src/main/java/life/fuyonghui/community/controller/IndexController.java
@@ -11,11 +11,10 @@ import org.springframework.web.bind.annotation.RequestParam;
  * @Version 1.0
  */
 @Controller
-public class HelloController {
+public class IndexController {
 
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name = "name") String name, Model model) {
-        model.addAttribute("name",name);
-        return "hello";
+    @GetMapping("/")
+    public String index() {
+        return "index";
     }
 }
