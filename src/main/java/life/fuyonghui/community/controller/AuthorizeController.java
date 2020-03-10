03bf0c94fefb41a5d87c31b53366f35cc7b727d2
wfufu <fu8926@qq.com>
2020-03-10 21:59:51 +0800

    add to application.properties

--- src/main/java/life/fuyonghui/community/controller/AuthorizeController.java
+++ src/main/java/life/fuyonghui/community/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import life.fuyonghui.community.dto.AccessTokenDTO;
 import life.fuyonghui.community.dto.GithubUser;
 import life.fuyonghui.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,19 +20,28 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    //去配置文件里读取key是github.client.id的value 把它赋值到clientId
+    //下同
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                             @RequestParam(name = "state") String state) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("eb5deaf097c8114b66fd");
-        accessTokenDTO.setClient_secret("92839f146f433c8c9aa7b4eba14d14a385263e99");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
 
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getId());
+        System.out.println(user.getName());
         return "index";
     }
 }
